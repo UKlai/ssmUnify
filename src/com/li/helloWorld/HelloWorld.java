8254a1f3a8c876aa0505a68599292aff5de60f2b
licong <1445586470@qq.com>
2020-06-09 19:59:17 +0800

    123

--- src/com/li/helloWorld/HelloWorld.java
+++ src/com/li/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.li.helloWorld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World2");
+		System.out.println("Hello Wo2rld2");
 	}
 }
